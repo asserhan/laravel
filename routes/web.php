f927620063fa60dde25a29b7fc082d5d385439cc
assehan <asseraouhanane@gmail.com>
2025-07-28 12:57:27 +0100

    add route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/hello', function () {
-    return 'hello world';
+    return response('<h1>hello world</h1>');
+   
 });
+
+Route::get('/posts/{id}',function($id){
+    return response('post '.$id);
+
+})->where('id', '[0-9]+');
